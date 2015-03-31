38f2fe02a45f764c69284f3fcbb1db4fbfacc8fc
Vladimir Bolshakov <vovanbo@gmail.com>
2015-03-31 09:49:04 +0300

    PostGIS DB creation is fixed with support of standard Postgres' docker environment variables.

--- initdb-postgis-database.sh
+++ initdb-postgis-database.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-gosu postgres postgres --single -E <<EOSQL
-CREATE ROLE postgis CREATEDB LOGIN PASSWORD 'postgis'
-CREATE DATABASE postgis TEMPLATE template_postgis OWNER postgis
+#!/bin/bash
+: ${POSTGRES_USER:=postgres}
+: ${POSTGRES_DB:=$POSTGRES_USER}
+
+gosu postgres pg_ctl start -w -D ${PGDATA}
+
+gosu postgres psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -d "$POSTGRES_DB" -U postgres <<EOSQL
+	CREATE EXTENSION postgis;
+	CREATE EXTENSION postgis_topology;
 EOSQL
+
+gosu postgres pg_ctl stop -w -D ${PGDATA}
